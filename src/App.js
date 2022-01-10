b046cfa13d90fc70ae62c240995c47f321c29b13
VitaliiKarpovets <k0rpsik@gmail.com>
2022-01-10 17:20:49 +0200

    final

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ import ContactsForm from "./components/contactsForm/ContactsForm";
 export default class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addNewContact = (contactName) => {
     if (
-      this.state.contacts.some((contact) => contact.name === contactName.name)
+      this.state.contacts.some((contact) => contact.name.toLowerCase() === contactName.name.toLowerCase())
     ) {
       return alert(`${contactName.name}  is already in contacts.`);
     } else {
